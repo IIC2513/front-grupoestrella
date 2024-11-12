da86bb74bd1cd2ce28d19edeaf3678dfcc605971
Stefano Arata <stefano.arata@uc.cl>
2024-11-12 17:21:49 -0300

    diseño

--- trivia/src/profile/Login.jsx
+++ trivia/src/profile/Login.jsx
@@ -1,9 +1,9 @@
+// Login.jsx
 import React, { useState, useContext } from 'react';
 import { AuthContext } from '../auth/AuthContext';
 import axios from 'axios';
 import './Login.css';
 import { useNavigate } from 'react-router-dom';
-
 
 function Login() {
   const { token, setToken } = useContext(AuthContext);
@@ -17,36 +17,35 @@ function Login() {
     event.preventDefault();
 
     axios.post(`${import.meta.env.VITE_BACKEND_URL}/login`, {
-        mail: mail,
-        password: password
-      }).then((response) => {
-        console.log('Login successful');
-        setError(false);
-        setMsg("Login exitoso!");
-        // Recibimos el token y lo procesamos
-        const access_token = response.data.access_token;
-        localStorage.setItem('token', access_token);
-        setToken(access_token);
-        console.log("Se seteo el token: ", token);
-        navigate('/loggedin'); 
-        
-      }).catch((error) => {
-        console.error('An error occurred while trying to login:', error);
-        setError(true);
-      })
+      mail: mail,
+      password: password
+    }).then((response) => {
+      console.log('Login successful');
+      setError(false);
+      setMsg("Login exitoso!");
+      const access_token = response.data.access_token;
+      localStorage.setItem('token', access_token);
+      setToken(access_token);
+      console.log("Se seteo el token: ", token);
+      navigate('/loggedin');
+    }).catch((error) => {
+      console.error('An error occurred while trying to login:', error);
+      setError(true);
+    });
   };
-
 
   return (
     <div className="Login">
+      <h1>Inicio de Sesión</h1> {/* Título agregado */}
+      
       {msg.length > 0 && <div className="successMsg"> {msg} </div>}
-
       {error && <div className="error">Hubo un error con el Login, por favor trata nuevamente.</div>}
+
       <form onSubmit={handleSubmit}>
         <label>
-          mail:
+          Correo:
           <input 
-            type="mail" 
+            type="email" 
             name="mail"
             value={mail}
             onChange={e => setMail(e.target.value)}
@@ -54,7 +53,7 @@ function Login() {
           />
         </label>
         <label>
-          Password:
+          Contraseña:
           <input 
             type="password" 
             name="password"
@@ -65,6 +64,9 @@ function Login() {
         </label>
         <input type="submit" value="Enviar" />
       </form>
+
+      {/* Botón Volver */}
+      <button className="button-link" onClick={() => navigate('/')}>Volver</button>
     </div>
   );
 }
